f66a64c0792f2e89c634026ca2e8f85b475346ed
Levani Papashvili <papashvililevani@gmail.com>
2020-02-10 16:26:57 -0500

    Done

--- script.js
+++ script.js
@@ -35,4 +35,78 @@ resetButton.addEventListener("click", function(e) {
     boxes[i].dataset.isClicked = "false";
   }
 });
+// this function is checking winner
+function checkWinner() {
+  console.log("checking for the  winner");
+  console.log(boxes);
+  console.log(boxes[0]);
+  console.log(boxes[0].style.backgroundColor);
+  if (
+    (boxes[0].style.backgroundColor === "red" &&
+      boxes[1].style.backgroundColor === "red" &&
+      boxes[2].style.backgroundColor === "red") ||
+    (boxes[0].style.backgroundColor === "red" &&
+      boxes[3].style.backgroundColor === "red" &&
+      boxes[6].style.backgroundColor === "red") ||
+    (boxes[0].style.backgroundColor === "red" &&
+      boxes[4].style.backgroundColor === "red" &&
+      boxes[8].style.backgroundColor === "red") ||
+    (boxes[1].style.backgroundColor === "red" &&
+      boxes[4].style.backgroundColor === "red" &&
+      boxes[7].style.backgroundColor === "red") ||
+    (boxes[2].style.backgroundColor === "red" &&
+      boxes[5].style.backgroundColor === "red" &&
+      boxes[8].style.backgroundColor === "red") ||
+    (boxes[3].style.backgroundColor === "red" &&
+      boxes[4].style.backgroundColor === "red" &&
+      boxes[5].style.backgroundColor === "red") ||
+    (boxes[2].style.backgroundColor === "red" &&
+      boxes[4].style.backgroundColor === "red" &&
+      boxes[6].style.backgroundColor === "red") ||
+    (boxes[6].style.backgroundColor === "red" &&
+      boxes[7].style.backgroundColor === "red" &&
+      boxes[8].style.backgroundColor === "red")
+  ) {
+    alert("RED WINS! Press reset and try again!");
+  } else if (
+    (boxes[0].style.backgroundColor === "blue" &&
+      boxes[1].style.backgroundColor === "blue" &&
+      boxes[2].style.backgroundColor === "blue") ||
+    (boxes[0].style.backgroundColor === "blue" &&
+      boxes[3].style.backgroundColor === "blue" &&
+      boxes[6].style.backgroundColor === "blue") ||
+    (boxes[0].style.backgroundColor === "blue" &&
+      boxes[4].style.backgroundColor === "blue" &&
+      boxes[8].style.backgroundColor === "blue") ||
+    (boxes[1].style.backgroundColor === "blue" &&
+      boxes[4].style.backgroundColor === "blue" &&
+      boxes[7].style.backgroundColor === "blue") ||
+    (boxes[2].style.backgroundColor === "blue" &&
+      boxes[5].style.backgroundColor === "blue" &&
+      boxes[8].style.backgroundColor === "blue") ||
+    (boxes[3].style.backgroundColor === "blue" &&
+      boxes[4].style.backgroundColor === "blue" &&
+      boxes[5].style.backgroundColor === "blue") ||
+    (boxes[2].style.backgroundColor === "blue" &&
+      boxes[4].style.backgroundColor === "blue" &&
+      boxes[6].style.backgroundColor === "blue") ||
+    (boxes[6].style.backgroundColor === "blue" &&
+      boxes[7].style.backgroundColor === "blue" &&
+      boxes[8].style.backgroundColor === "blue")
+  ) {
+    alert("BLUE WINS! Press reset and try  again!");
+  } else if (
+    boxes[0].style.backgroundColor !== "" &&
+    boxes[1].style.backgroundColor !== "" &&
+    boxes[2].style.backgroundColor !== "" &&
+    boxes[3].style.backgroundColor !== "" &&
+    boxes[4].style.backgroundColor !== "" &&
+    boxes[5].style.backgroundColor !== "" &&
+    boxes[6].style.backgroundColor !== "" &&
+    boxes[7].style.backgroundColor !== "" &&
+    boxes[8].style.backgroundColor !== ""
+  ) {
+    alert("It's a TIE! Press reset and try  again!");
+  }
+}
 
